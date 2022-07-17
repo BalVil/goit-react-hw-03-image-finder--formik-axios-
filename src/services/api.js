d20cf1ad45dc79a2aada030615f5954dc7b1074b
BalVil <balutavili@gmail.com>
2022-07-17 22:41:56 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = 'key=27754305-6c0117069a54d6a4ab2d99661';
-const PER_PAGE = 'per_page=12';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export const fetchImages = async (query, page) => {
-  const url = `${BASE_URL}?q=${query}&page=${page}&${API_KEY}&image_type=photo&orientation=horizontal&${PER_PAGE}`;
+  const PER_PAGE = 'per_page=12';
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(
+      `/?q=${query}&page=${page}&${API_KEY}&image_type=photo&orientation=horizontal&${PER_PAGE}`
+    );
     return response.data;
   } catch (error) {
     console.log(error);
